193a483d078eaf3f6533767f5af2a6016505caa5
Dikkco <74995401+Dikkco@users.noreply.github.com>
2020-12-07 10:01:40 +0800

    修改字段判空条件.

--- src/Extend/common.php
+++ src/Extend/common.php
@@ -3,6 +3,8 @@
 if (!function_exists('param_exist')) {
     function param_exist(array $params, string $key)
     {
-        return (isset($params[$key]) && $params[$key] !== '') ? true : false;
+        return isset($params[$key]) 
+            && $params[$key] !== '' 
+            && $params[$key] !== [];
     }
 }
